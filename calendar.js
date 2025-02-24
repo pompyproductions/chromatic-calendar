781555e560f097a86be1bc614d1d7edfb0a5f70d
Eren Gazioglu <accounts@pompyproductions.com>
2025-02-24 21:06:07 +0100

    Create elements through JS

--- calendar.js
+++ calendar.js
@@ -1,1 +1,28 @@
 const weeksContainer = document.getElementById("weeks-container")
+
+function createRow(firstDay) {
+  const rowElem = document.createElement("div");
+  rowElem.classList.add("row");
+  rowElem.setAttribute("data-week", 0);
+  
+  const weekElem = document.createElement("ol");
+  weekElem.classList.add("week");
+  rowElem.append(weekElem);
+
+  for (let i = firstDay; i < firstDay + 7; i++) {
+    const dayElem = document.createElement("li");
+    dayElem.classList.add("even");
+    if (i > firstDay + 4) {
+      dayElem.classList.add("weekend");
+    }
+    dayElem.textContent = i.toString().padStart(2, "0");
+    weekElem.append(dayElem);
+  }
+
+  return rowElem;
+}
+
+
+for (let i = 1; i < 4; i++) {
+  weeksContainer.append(createRow(i * 7 - 6));
+}
